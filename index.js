3ee2ccca5827d4466a6f0504ec7fc29df79345e5
Jeremiah Diboll <jdiboll@gmail.com>
2017-08-02 18:11:23 -0400

    It's alive... and it works.

--- index.js
+++ index.js
@@ -13,7 +13,58 @@ app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-// const placeholder = '_'
+const guess = []
+const word = words[Math.floor(Math.random(words) * words.length)]
+console.log(word)
+const wordLength = word.split('')
+const placeholder = wordLength.map(x => {
+  return '_'
+})
+let count = 8
+
+app.get('/', (req, res) => {
+  res.render('index', { placeholder: placeholder, guess: guess, count: count })
+})
+app.get('/win', (req, res) => {
+  res.render('win', { guess: guess, count: count, word: word })
+})
+app.get('/lose', (req, res) => {
+  res.render('lose', { guess: guess, count: count, word: word })
+})
+
+app.post('/guess', (req, res) => {
+  guess.push(req.body.guess)
+  if (wordLength.includes(req.body.guess)) {
+    wordLength.forEach((letter, index) => {
+      if (letter === req.body.guess) {
+        placeholder[index] = letter
+      }
+    })
+  } else {
+    count -= 1
+    if (placeholder.join(',') != wordLength.join(',') && count <= 0) {
+      message = 'You lose!'
+      console.log(message)
+      res.redirect('/lose')
+    }
+  }
+  if (placeholder.join(',') === wordLength.join(',') && count >= 0) {
+    message = 'You win!'
+    console.log(message)
+    res.redirect('/win')
+  }
+  res.redirect('/')
+})
+app.post('/win', (req, res) => {
+  res.render('/index')
+})
+app.post('/lose', (req, res) => {
+  res.render('/index')
+})
+
+app.listen(3000, () => {
+  console.log("Let's do this!")
+})
 // const hangman = (word, guesses) => {
 //   let output = ''
 //   for (let i = 0; i < word.length; i++) {
@@ -25,46 +76,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 //     }
 //   }
 //   return output
-//   //   This is the advanced version of the above code
-//   //   I'm not at this level yet, but i kind of understand it.
-//   //   return word
-//   //     .split('')
-//   //     .map(letter => {
-//   //       return guesses.include(letter) ? letter : placeholder
-//   //     })
-//   //     .join('')
+//   This is the advanced version of the above code
+//   I'm not at this level yet, but i kind of understand it.
+//   return word
+//     .split('')
+//     .map(letter => {
+//       return guesses.include(letter) ? letter : placeholder
+//     })
+//     .join('')
 // }
-const guesses = ''
-
-app.get('/', (req, res) => {
-  const word = words[Math.floor(Math.random(words) * words.length)]
-  console.log(word)
-  //then display the right amount of spaces for the word
-
-  //let the player know that they have 8 guesses to start
-  res.render('index')
-})
-
-app.post('/guess', (req, res) => {
-  const hangman = (word, guesses) => {
-    let output = ''
-    for (let i = 0; i < word.length; i++) {
-      const letter = word[i]
-      if (guesses.include(letter)) {
-        output += letter
-      } else {
-        output += placeholder
-      }
-    }
-    return output
-  }
-  //once you submit a guess it will add that guess to the Letters Guessed
-  //it will also check the word to see if that letter is in the word
-  //then it will return the word with the chosen letters revealed
-  //it will also tell you how many guesses you have left
-  res.redirect('/', output)
-})
-
-app.listen(3000, () => {
-  console.log("Let's do this!")
-})
